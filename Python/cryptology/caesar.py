b6af3d7b8de1e0c309b7f02ea4387c2c6098032c
lambs2slaughter <q2f7rhh2z7@privaterelay.appleid.com>
2025-11-28 16:02:31 -0500

    abstraction

--- Python/cryptology/caesar.py
+++ Python/cryptology/caesar.py
@@ -1,11 +1,12 @@
 import string
 
-global_shift = 0
-global_plainText = ''
-global_alphabet = string.ascii_lowercase
-
-def Caesar(shift, plainText, alphabet):
-    def shift_alphabet(alphabet):
-        return alphabet[shift:]+alphabet[:shift]
-    def encrypt():
-        table = map()
+plainText = str(input("What is the plain text value? "))
+shift = int(input("What is the amount to shift? "))
+shift %= 26
+    
+def caesar(plainText: str, shit: int):
+  alphabet = string.ascii_letters
+  def shifted_alphabet():
+    return alphabet[shift:]+alphabet[:shift]
+  print(shifted_alphabet())
+caesar(plainText, shift)
